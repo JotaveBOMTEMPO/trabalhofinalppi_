f6e0952c23d29be62cac12198646c2eee3174c07
João Victor Bomtempo Costa <bomtempojoaovictor2005@gmail.com>
2023-12-13 18:56:13 -0300

    commit

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ else {
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Cadastro de Usuário</title>
+            <title>Cadastrados</title>
             <link rel="stylesheet" href="jaCadastradosStyle.css">
         </head>
         <body>
